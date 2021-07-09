af44265f141840e53bf4ce302afee33e44d0c55d
Wayne0926 <wayne0926@foxmail.com>
2021-07-09 23:12:41 +0800

    代码优化

--- jsd.py
+++ jsd.py
@@ -2,9 +2,8 @@ import requests
 url = input('输入jsd的CDN链接：')
 url = url.replace('cdn.jsdelivr.net', 'purge.jsdelivr.net')
 print('正在请求链接：' + url)
-num = 1
-while num <= 4:
-    t = requests.get(url).json()
-    print('请求了第' + str(num) + '次')
-    num = num + 1
+for times in range(4):
+    result = requests.get(url).json()['success']
+    print('请求了第' + str(times+1) + '次')
+    print(result)
 print('完成')
